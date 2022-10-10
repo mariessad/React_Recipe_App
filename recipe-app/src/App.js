eac830b1df4049c2e3c6a473e681165fdde0f88c
mariessad <74148632+mariessad@users.noreply.github.com>
2022-10-10 14:20:53 -0400

    add form with search input and submit button, add function to submit button to run query function and prevent default pg refresh

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const { REACT_APP_ID, REACT_APP_KEY } = process.env;
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_ID}&app_key=${REACT_APP_KEY}&&health=alcohol-free`;
+
+  async function getRecipies() {
+    // installed axios, must be imported to use
+    let result = await Axios.get(url);
+    console.log(result.data);
+  }
+
+  const submit = (e) =>{
+    e.preventDefault();
+    getRecipies();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipe App</h1>
+      <form className="app-search-form" onSubmit={submit}>
+        <input
+          type="text"
+          className="app-search-input"
+          placeholder="enter ingredient"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input className="app-search-button" type="submit" value="Search" />
+      </form>
     </div>
   );
 }
