20f6bd3c343748dff0dc9adfe3ce1b353008b744
mariessad <74148632+mariessad@users.noreply.github.com>
2022-10-10 18:29:55 -0400

    edit CSS, add Logo Image, add useEffect structure

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,20 +1,26 @@
 import "./App.css";
 import Axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RecipeCard from "./RecipeCard";
+import plate from "./plate-logo.png";
 
 function App() {
   const [query, setQuery] = useState("");
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
+  const [health, setHealth] = useState("vegan");
+
+  useEffect(() => {
+    console.log("render");
+  }, [query]);
 
   const { REACT_APP_ID, REACT_APP_KEY } = process.env;
-  let url = `https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_ID}&app_key=${REACT_APP_KEY}&&health=alcohol-free`;
+  let url = `https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_ID}&app_key=${REACT_APP_KEY}&health=${health}`;
 
   async function getRecipies() {
     // installed axios, must be imported to use
-    
+
     let result = await Axios.get(url);
-    setRecipes(result.data.hits)
+    setRecipes(result.data.hits);
     console.log(result.data);
   }
 
@@ -24,9 +30,17 @@ function App() {
   };
   return (
     <div className="App">
-      <h1>Recipe App</h1>
+      <header>
+        <h1>What2Cook</h1>
+        <p>
+          <img className="plate-logo" src={plate}></img>
+        </p>
+        <p>Find your new go-to meal</p>
+      </header>
+
       <form className="app-search-form" onSubmit={submit}>
         <input
+          required
           type="text"
           className="app-search-input"
           placeholder="enter ingredient"
@@ -34,10 +48,23 @@ function App() {
           onChange={(e) => setQuery(e.target.value)}
         />
         <input className="app-search-button" type="submit" value="Search" />
+        <select className="app-healthLables">
+          <option onClick={() => setHealth("")}>none</option>
+          <option onClick={() => setHealth("vegetarian")}>Vegan</option>
+          <option onClick={() => setHealth("vegan")}>Vegetarian</option>
+          <option onClick={() => setHealth("paleo")}>Paleo</option>
+          <option onClick={() => setHealth("dairy-free")}>Dairy-free</option>
+          <option onClick={() => setHealth("gluten-free")}>Gluten-free</option>
+          <option onClick={() => setHealth("wheat-free")}>Wheat-free</option>
+          <option onClick={() => setHealth("low-sugar")}>Low-sugar</option>
+          <option onClick={() => setHealth("egg-free")}>Egg-free</option>
+          <option onClick={() => setHealth("fish-free")}>Fish-free</option>
+        </select>
       </form>
+
       <div className="app-recipes">
-        {recipes.map(recipe =>{
-          return <RecipeCard recipe={recipe}/>
+        {recipes.map((recipe) => {
+          return <RecipeCard recipe={recipe} />;
         })}
       </div>
     </div>
--- recipe-app/src/RecipeCard.js
+++ recipe-app/src/RecipeCard.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import "./recipeCard.css"
+import React from "react";
+import "./recipeCard.css";
 
 export default function RecipeCard({ recipe }) {
   return (
-    <div className='recipeCard'>
-        <p className='recipe-card-label'>{recipe["recipe"]["label"]}</p>
-        <img className="recipe-card-image" src={recipe["recipe"]["image"]}/>
+    <div className="recipeCard">
+      <a href={recipe["recipe"]["url"]}>
+        <p className="recipe-card-label">{recipe["recipe"]["label"]}</p>
+
+        <img className="recipe-card-image" src={recipe["recipe"]["image"]} />
+
+        <p className="recipe-card-time">Cook Time: {recipe["recipe"]["totalTime"]} Mins</p>
+      </a>
     </div>
-  )
+  );
 }
